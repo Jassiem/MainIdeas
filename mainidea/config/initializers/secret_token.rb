1bc201e8f7ce1897dfa7cdec2439f3e5c1ab3048
Jassiem Moore <jassiemmoore@gmail.com>
2014-06-15 16:23:53 -0400

    Rails skeleton.

---
+++ mainidea/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mainidea::Application.config.secret_key_base = '543d407576cb5b9c2ac6e4f913441a1e5c7e3185ac900e040e284f592bc40710e53c52e693b3c3140f35c40f0c678057ce61a84f7af8561d665ff7690a80666b'
